9e7859c6b5454a35947223ece3232996ee043d96
Mikael Shumacer <mikaelabrell50@gmail.com>
2025-09-04 13:55:40 -0300

    adicionar view

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const exit_adicionar = document.getElementById("exit-adicionar");
-const div_adicionar = document.getElementById('adicionar-div');
-const bttn_adicionar = document.getElementById('button-add');
+const div_adicionar = document.getElementById("adicionar-div");
+const bttn_adicionar = document.getElementById("button-add");
+const lista_livros = document.getElementById("lista-livros");
+const nav_bar = document.getElementById("nav");
 
-div_adicionar.style.display = 'none';
+div_adicionar.style.display = "none";
 
-bttn_adicionar.addEventListener('click', function() {
-    div_adicionar.style.display = 'block';
+bttn_adicionar.addEventListener("click", function () {
+  div_adicionar.style.display = "block";
+  lista_livros.style.display = "none";
+  nav_bar.style.display = "none";
 });
 
-exit_adicionar.addEventListener('click', function() {
-    div_adicionar.style.display = 'none';
+exit_adicionar.addEventListener("click", function () {
+  div_adicionar.style.display = "none";
+  lista_livros.style.display = "";
+  nav_bar.style.display = "";
 });
